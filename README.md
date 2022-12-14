c8eea5b9773e7e86c7249a68a89a5a7e987d61f2
Javier Henao Hernandez <javier.henao@outlook.com>
2022-12-14 11:35:34 -0500

    ejercicio 3, primera parte

---
+++ README.md
@@ -0,0 +1,10 @@
+## Ejercicios tema 3
+
+Ejercicio introduccion a la programación.
+
+## Primera Parte:
+
+Crear una función con tres parámetros que sean números que se suman entre sí.
+
+Llamar a la función en el main y darle valores.
+
---
+++ src/App.java
@@ -0,0 +1,17 @@
+
+//import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        int result;
+        result = suma (1, 2, 3);
+        System.out.println(result);
+    }
+    public static int suma(int a, int b, int c) {
+        return a+b+c;
+        
+    }
+}
+
+
+
